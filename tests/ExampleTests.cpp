9cf12c34b36575c4cce5cf7fb006ea0153d9801f
Miro Loisa <N4187@student.jamk.fi>
2024-12-05 18:10:10 +0200

    Implements tests for resource manager

--- tests/ExampleTests.cpp
+++ tests/ExampleTests.cpp
@@ -1,7 +1,53 @@
 #include <gtest/gtest.h>
 #include "ResourceManager.hpp"
+#include "WindowManager.hpp"
 
 
+class ResourceManagerTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Set up the resource manager with a test configuration
+        test_config.textureCellSize = 16;
+        test_config.textureResolution = 128;
+        test_config.texturesPath = "resources/textures";
+        test_config.fontsPath = "resources/fonts";
+
+        resourceManager = &ResourceManager::getInstance(test_config);
+    }
+
+    ResourceManagerConfig test_config;
+    ResourceManager* resourceManager;
+};
+
+// Test ResourceManager initialization
+TEST_F(ResourceManagerTest, Initialization) {
+    EXPECT_EQ(resourceManager->getConfig().textureCellSize, 16);
+    EXPECT_EQ(resourceManager->getConfig().textureResolution, 128);
+    EXPECT_EQ(resourceManager->getConfig().texturesPath, "resources/textures");
+    EXPECT_EQ(resourceManager->getConfig().fontsPath, "resources/fonts");
+}
+
+// Test checkerboard texture creation
+TEST_F(ResourceManagerTest, CreateCheckerboardTexture) {
+    sf::Texture texture = resourceManager->createCheckerboardTexture(test_config);
+    sf::Image image = texture.copyToImage();
+
+    // Check some pixels to ensure the checkerboard pattern
+    EXPECT_EQ(image.getPixel(0, 0), sf::Color::Magenta);
+    EXPECT_EQ(image.getPixel(1, 0), sf::Color::Black);
+    EXPECT_EQ(image.getPixel(0, 1), sf::Color::Black);
+    EXPECT_EQ(image.getPixel(1, 1), sf::Color::Magenta);
+    EXPECT_EQ(texture.getSize().x, 128);
+    EXPECT_EQ(texture.getSize().y, 128);
+}
+
+// Test sprite creation
+TEST_F(ResourceManagerTest, CreateSprite) {
+    sf::Texture test_texture = resourceManager->getErrorTexture();
+    sf::Sprite sprite = resourceManager->createSprite(1, test_texture);
+    EXPECT_EQ(sprite.getTextureRect().width, 16); // Ensure the texture rectangle is set correctly
+    EXPECT_EQ(sprite.getTextureRect().height, 16);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
