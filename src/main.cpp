851d0be2992c63eec0e3cf8e4d1cebfeb0ab9462
Miro Loisa <N4187@student.jamk.fi>
2024-12-06 19:48:22 +0200

    some testing of grid

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,45 @@ int main()
     resourceManager.loadResources();
     std::cout << resourceManager << "\n";
     Instrumentor::Get().BeginSession("Profile");
-    std::vector<sf::Sprite> sprites;
     
+        //     sprite.scale(5.0f, 5.0f);
+        
+        // // Set the position with an offset
+        // float xOffset = 10.0f; // Adjust this value as needed
+        // float yOffset = 10.0f; // Adjust this value as needed
+        // sprite.setPosition(i * xOffset, i * yOffset);
+    std::vector<sf::Sprite> sprites;    
+    sprites.reserve(1000); // Pre-allocate memory for 1000 elements
 
-    for (int i = 0; i < 1000; ++i) {
-        sf::Sprite sprite = resourceManager.createSprite(i, resourceManager.getTexture("dsadasd.png"));
-        sprite.scale(5.0f, 5.0f);
-        
-        // Set the position with an offset
-        float xOffset = 10.0f; // Adjust this value as needed
-        float yOffset = 10.0f; // Adjust this value as needed
-        sprite.setPosition(i * xOffset, i * yOffset);
-        
-        sprites.push_back(sprite);
+    {
+        PROFILE_SCOPE("Create sprite test");
+        struct GridConfig {
+            int rows;
+            int columns;
+            float spacing; // Add spacing between tiles
+        };
+
+        GridConfig gridConfig = {100, 100, 5.0f}; // Adjust these values as needed
+
+        sf::Texture& texture = resourceManager.getTexture("dsadasd.png");
+       
+
+        for (int row = 0; row < gridConfig.rows; ++row) {
+            for (int col = 0; col < gridConfig.columns; ++col) {
+            sf::Sprite sprite = resourceManager.createSprite(1, texture);
+            sf::FloatRect spriteSize = sprite.getLocalBounds();
+            sprite.setPosition(col * (spriteSize.height + gridConfig.spacing), row * (spriteSize.width + gridConfig.spacing));
+            sprites.push_back(sprite);
+            
+            }
+        }
+
     }
-    
-    
     WindowManager windowManager;
     windowManager.updateSpriteArray(std::move(sprites));
-    windowManager.run();
     
+    windowManager.run();
+  
     Instrumentor::Get().EndSession();
     
     return 0;
