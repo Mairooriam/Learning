f55d681e2dc1f3ba4d5fe5c0b29da196a237bae8
Miro Loisa <N4187@student.jamk.fi>
2024-12-30 02:20:57 +0200

    added events + layer

--- Mir/src/Mir/application.h
+++ Mir/src/Mir/application.h
@@ -1,17 +1,29 @@
 #pragma once
 
-#include "Mir/core.h"
-#include "Events/Event.h"
+#include "core.h"
+
 #include "Window.h"
+#include "Mir/LayerStack.h"
+#include "Mir/Events/ApplicationEvent.h"
+#include "Mir/Events/Event.h"
+
 namespace Mir {
     class MIR_API Application {
     public:
         Application();
         virtual ~Application();
         void Run();
+
+        void OnEvent(Event& e);
+
+        void PushLayer(Layer* layer);
+        void PushOverlay(Layer* layer);
     private:
+        bool OnwWindowClose(WindowCloseEvent& e);
+
         std::unique_ptr<Window> m_Window;
         bool m_Running = true;
+        LayerStack m_LayerStack;
     };
 
     Application* createApplication();
