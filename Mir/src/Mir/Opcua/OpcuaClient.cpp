16c4b7907ab5d714757c1b64ac4f20e756d94937
Miro Loisa <N4187@student.jamk.fi>
2025-02-14 00:05:26 +0200

    started adding data

--- Mir/src/Mir/Opcua/OpcuaClient.cpp
+++ Mir/src/Mir/Opcua/OpcuaClient.cpp
@@ -9,49 +9,10 @@
 #include <open62541pp/node.hpp>
 
 
-
 namespace Mir {
 
-    /// Get name of NodeIdType.
-    constexpr std::string_view toString(opcua::NodeIdType nodeIdType) {
-        switch (nodeIdType) {
-        case opcua::NodeIdType::Numeric:
-            return "Numeric";
-        case opcua::NodeIdType::String:
-            return "String";
-        case opcua::NodeIdType::Guid:
-            return "Guid";
-        case opcua::NodeIdType::ByteString:
-            return "ByteString";
-        default:
-            return "Unknown";
-        }
-    }
-    /// Get name of VariableType.
-    constexpr std::string_view toString(opcua::NodeClass variableType) {
-        switch (variableType) {
-        case opcua::NodeClass::Unspecified:
-            return "Unspecified";
-        case opcua::NodeClass::Object:
-            return "Object";
-        case opcua::NodeClass::Variable:
-            return "Variable";
-        case opcua::NodeClass::Method:
-            return "Method";
-        case opcua::NodeClass::ObjectType:
-            return "ObjectType";
-        case opcua::NodeClass::VariableType:
-            return "VariableType";
-        case opcua::NodeClass::ReferenceType:
-            return "ReferenceType";
-        case opcua::NodeClass::DataType:
-            return "DataType";
-        case opcua::NodeClass::View:
-            return "View";
-        default:
-            return "Unknown";
-        }
-    }
+
+
 
     // Separate definition for recursion
     void printNodeTree(opcua::Node<opcua::Client>& node, int indent);
@@ -107,25 +68,26 @@ namespace Mir {
         //opcua::Node nodeRoot(m_client, opcua::ObjectId::RootFolder);
         //browseNode(nodeRoot, 5);
 
+        
         std::vector<std::string> test = m_client.namespaceArray();
         // Create a node with a specific NodeId instead of the RootFolder
-        opcua::Node node(m_client, opcua::NodeId(4, 2));
-        opcua::Node rootNode(m_client, opcua::ObjectId::RootFolder);
+        opcua::Node node(m_client, opcua::NodeId(3, "PLC"));
+        //opcua::Node rootNode(m_client, opcua::ObjectId::RootFolder);
         // Call the browseChildren method and save the result to a variable
-        std::vector<opcua::Node<opcua::Client>> children = rootNode.browseChildren();
+        std::vector<opcua::Node<opcua::Client>> children = node.browseChildren();
 
 
-        const auto& nodeId = children[10].id();
+        // const auto& nodeId = children[10].id();
 
-        opcua::NamespaceIndex NamespaceIndex = nodeId.namespaceIndex();
-        opcua::NodeIdType NodeId = nodeId.identifierType();
-        auto indentifier = nodeId.identifier<uint32_t>();
-        std::string nodeIdString = nodeId.toString();
-        auto datattypepe = children[10].readNodeClass();
+        // opcua::NamespaceIndex NamespaceIndex = nodeId.namespaceIndex();
+        // opcua::NodeIdType NodeId = nodeId.identifierType();
+        // auto indentifier = nodeId.identifier<uint32_t>();
+        // std::string nodeIdString = nodeId.toString();
+        // auto datattypepe = children[10].readNodeClass();
         
 
 
-        opcua::QualifiedName teest1 = children[10].readBrowseName();
+        // opcua::QualifiedName teest1 = children[10].readBrowseName();
 
             // Store nodes in the map
         for (auto& child : children) {
@@ -134,14 +96,8 @@ namespace Mir {
             auto childhandle = childid.handle();
             auto DisplayName4 = child.readDisplayName().typeIndex();
             auto displayname2 = child.readDisplayName().locale();
-
-            Mir::NodeId nodeid{
-                childid,
-                static_cast<uint16_t>(childid.namespaceIndex()),
-                std::string(toString(nodeidtype)),
-                childid.identifier<uint32_t>(), // will cause bug at somepoinmt not considering other types
-
-            };
+            
+            Mir::NodeId nodeid(childid);
 
             auto BrowseName = child.readBrowseName(); // example 4, "publis_lenght"
             auto DisplayName = child.readDisplayName();
@@ -149,9 +105,31 @@ namespace Mir {
 
             opcua::Variant DataValue;
 
+            
             try { 
-                DataValue = child.readValue();
+                auto Data = child.readDataValue();
+                // GET VALUE AND ITS DATA //
+                DataValue = Data.value();
                 
+                
+                const UA_DataType* dataType = DataValue.type();
+                
+                auto data = DataValue.data();
+            
+                if (dataType == &UA_TYPES[UA_TYPES_INT32]) {
+                    int32_t value = *static_cast<int32_t*>(data);
+                    // Use the value as needed
+                } else if (dataType == &UA_TYPES[UA_TYPES_DOUBLE]) {
+                    double value = *static_cast<double*>(data);
+                    // Use the value as needed
+                } else if (dataType == &UA_TYPES[UA_TYPES_STRING]) {
+                    
+                    opcua::String value = *static_cast<opcua::String*>(data);
+                    std::string stringValue(value.data(), value.length());
+                    // Use the stringValue as needed
+                } else {
+                    // Handle other data types as needed
+                }
                 // // Check the data type and cast accordingly
                 // if (DataValue.type() == opcua::VariantType::Int32) {
                 //     int32_t value = *static_cast<int32_t*>(DataValue.data());
@@ -188,12 +166,12 @@ namespace Mir {
         }
 
 
-        opcua::Bitmask<opcua::AccessLevel> AccesLevel = children[6].readAccessLevel();
-        //std::vector<uint32_t> ArrayDimensions = children[4].readArrayDimensions();
-        opcua::QualifiedName BrowseName = children[6].readBrowseName();
-        opcua::NodeId DataType = children[6].readDataType();
-        //opcua::Variant DataTypeDefinition = children[6].readDataTypeDefinition();
-        opcua::DataValue DataValue = children[6].readDataValue();
+        // opcua::Bitmask<opcua::AccessLevel> AccesLevel = children[6].readAccessLevel();
+        // //std::vector<uint32_t> ArrayDimensions = children[4].readArrayDimensions();
+        // opcua::QualifiedName BrowseName = children[6].readBrowseName();
+        // opcua::NodeId DataType = children[6].readDataType();
+        // //opcua::Variant DataTypeDefinition = children[6].readDataTypeDefinition();
+        // opcua::DataValue DataValue = children[6].readDataValue();
         //opcua::LocalizedText Description = children[6].readDescription();
 
         // for (opcua::Node<opcua::Client>& child : children) {
--- Mir/src/Mir/Opcua/OpcuaClient.h
+++ Mir/src/Mir/Opcua/OpcuaClient.h
@@ -8,13 +8,79 @@
 #include <open62541pp/node.hpp>
 namespace Mir {
 
+	    /// Get name of NodeIdType.
+		constexpr std::string_view toString(opcua::NodeIdType nodeIdType) {
+			switch (nodeIdType) {
+			case opcua::NodeIdType::Numeric:
+				return "Numeric";
+			case opcua::NodeIdType::String:
+				return "String";
+			case opcua::NodeIdType::Guid:
+				return "Guid";
+			case opcua::NodeIdType::ByteString:
+				return "ByteString";
+			default:
+				return "Unknown";
+			}
+		}
+		/// Get name of VariableType.
+		constexpr std::string_view toString(opcua::NodeClass variableType) {
+			switch (variableType) {
+			case opcua::NodeClass::Unspecified:
+				return "Unspecified";
+			case opcua::NodeClass::Object:
+				return "Object";
+			case opcua::NodeClass::Variable:
+				return "Variable";
+			case opcua::NodeClass::Method:
+				return "Method";
+			case opcua::NodeClass::ObjectType:
+				return "ObjectType";
+			case opcua::NodeClass::VariableType:
+				return "VariableType";
+			case opcua::NodeClass::ReferenceType:
+				return "ReferenceType";
+			case opcua::NodeClass::DataType:
+				return "DataType";
+			case opcua::NodeClass::View:
+				return "View";
+			default:
+				return "Unknown";
+			}
+		}
 
-    struct NodeId{
-        opcua::NodeId nodeIdHandle;
-        uint16_t NamespaceIndex;
-        std::string identifierType;
-        uint32_t identifier;
-    };
+		struct NodeId {
+			opcua::NodeId nodeIdHandle;
+			uint16_t NamespaceIndex;
+			std::string identifierType;
+			std::variant<uint32_t, std::string, opcua::Guid, opcua::ByteString> identifier;
+		
+			// Default constructor
+			NodeId()
+				: nodeIdHandle(), NamespaceIndex(0), identifierType(""), identifier(uint32_t(0)) {}
+		
+			NodeId(const opcua::NodeId& nodeId)
+				: nodeIdHandle(nodeId),
+					NamespaceIndex(static_cast<uint16_t>(nodeId.namespaceIndex())),
+					identifierType(std::string(toString(nodeId.identifierType()))),
+					identifier(extractIdentifier(nodeId)) {}
+		
+		private:
+			static std::variant<uint32_t, std::string, opcua::Guid, opcua::ByteString> extractIdentifier(const opcua::NodeId& nodeId) {
+				switch (nodeId.identifierType()) {
+					case opcua::NodeIdType::Numeric:
+						return nodeId.identifier<uint32_t>(); // TODO: FIX THESE LIKE THE STRING
+					case opcua::NodeIdType::String:
+                        return std::string(nodeId.identifier<opcua::String>().data(), nodeId.identifier<opcua::String>().length()); // will call identifier two times not good? might be performance problem
+					case opcua::NodeIdType::Guid:
+						return nodeId.identifier<opcua::Guid>(); // TODO: FIX THESE LIKE THE STRING
+					case opcua::NodeIdType::ByteString:
+						return nodeId.identifier<opcua::ByteString>(); // TODO: FIX THESE LIKE THE STRING
+					default:
+						throw std::runtime_error("Unsupported NodeIdType");
+				}
+			}
+		};
 
     struct NodeData {
         Mir::NodeId NodeId;
